f449b71718799aedc48eb8d5bcc8d41562afdb66
daniel-dev-cyber <aljudaniel99@gmail.com>
2026-05-23 22:09:13 -0500

    Add files via upload

---
+++ Triangulos.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Triangulos {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        if (a + b > c && a + c > b && b + c > a) {
+
+            if (a == b && b == c)
+                System.out.println("Equilátero");
+
+            else if (a == b || a == c || b == c)
+                System.out.println("Isósceles");
+
+            else
+                System.out.println("Escaleno");
+
+        } else {
+            System.out.println("No válido");
+        }
+
+        sc.close();
+    }
+}
